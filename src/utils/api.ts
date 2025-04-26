5d6dfb483a4d412bf4c808ef2feb7313b3b2c416
LoreviQ <lorevi@protonmail.com>
2025-04-26 15:07:10 +0100

    made api directl in code

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
-const API_VERSION = '/v1';
+const API_BASE_URL = import.meta.env.PROD
+    ? 'https://baxus-bob-1037939514360.europe-west1.run.app'
+    : 'http://localhost:3000';
+const API_VERSION = '/api/v1';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            '/api': {
-                target:
-                    mode === 'production'
-                        ? 'https://baxus-bob-1037939514360.europe-west1.run.app/'
-                        : 'http://localhost:3000',
-                changeOrigin: true,
-            },
-        },
-    },
-    build: {
-        sourcemap: true,
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                // Preserve console.logs for debugging
-                pure_funcs: [],
-                keep_fnames: true,
-            },
-            mangle: {
-                keep_fnames: true,
-            },
-        },
-    },
-}));
+});
